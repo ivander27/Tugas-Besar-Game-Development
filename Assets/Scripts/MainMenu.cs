0848783c38ff15cf6b6651bd0b5c6cc246832b83
ivander27 <71996385+ivander27@users.noreply.github.com>
2022-12-12 16:21:07 +0700

    Upload Files

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string levelToLoad = "MainLevel";
+
+	public SceneFader sceneFader;
+
+	public void Play ()
+	{
+		sceneFader.FadeTo(levelToLoad);
+	}
+
+	public void Setting ()
+	{
+		SceneManager.LoadScene("Setting");
+	}
+
+	public void Quit ()
+	{
+		Debug.Log("Exciting...");
+		Application.Quit();
+	}
+
+}
